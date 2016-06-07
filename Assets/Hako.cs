ecbbf742f5ad5c1a780e815a9d91848aa94b6e36
Ayaminn <ayamin.imai@gmail.com>
2016-06-07 22:31:31 +0900

    カメラをハコスコ仕様に変更・Mainスクリプトの削除

--- Assets/Hako.cs
+++ Assets/Hako.cs
@@ -1,1 +1,19 @@
-﻿using UnityEngine; using System.Collections;  public class Hako : MonoBehaviour {  	// Use this for initialization 	void Start () {  	}  	// Update is called once per frame 	void Update () { 		Quaternion q = Input.gyro.attitude; 		Quaternion qq = Quaternion.AngleAxis(-90.0f, Vector3.left); 		q.x *= -1.0f; 		q.y *= -1.0f; 		this.transform.localRotation = qq * q; 	} }
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hako : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion q = Input.gyro.attitude;
+		Quaternion qq = Quaternion.AngleAxis(-90.0f, Vector3.left);
+		q.x *= -1.0f;
+		q.y *= -1.0f;
+		this.transform.localRotation = qq * q;
+	}
+}
