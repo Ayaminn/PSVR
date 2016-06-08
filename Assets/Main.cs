4e324e26b60e1274b5b9671c514ded53945e4a61
Ayaminn <ayamin.imai@gmail.com>
2016-06-08 18:12:54 +0900

    Switch Platform

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -13,18 +13,18 @@ public class Main : MonoBehaviour {
 		float axis3 = Input.GetAxis("axis3");
 		transform.position += new Vector3 (axis3, 0, 0);
 //		Debug.Log (Input.GetAxis ("Horizontal"));
-//		transform.position = new Vector3 (transform.position.x + Input.GetAxis ("Horizontal"), transform.position.y, transform.position.z);
+		transform.position = new Vector3 (transform.position.x + Input.GetAxis ("Horizontal"), transform.position.y, transform.position.z);
 
-//		if(Input.GetAxisRaw("Horizontal") < 0){
-//			//左に傾いている
-//			transform.position += new Vector3(-0.1f, 0, 0);
-//		}else if(0 < Input.GetAxisRaw("Horizontal")){
-//			//右に傾いている
-//			transform.position += new Vector3(0.1f, 0, 0);
-//		}else{
-//			//左右方向には傾いていない
-//		}
+		if(Input.GetAxisRaw("Horizontal") < 0){
+			//左に傾いている
+			transform.position += new Vector3(-0.1f, 0, 0);
+		}else if(0 < Input.GetAxisRaw("Horizontal")){
+			//右に傾いている
+			transform.position += new Vector3(0.1f, 0, 0);
+		}else{
+			//左右方向には傾いていない
+		}
 
-//		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * 30, 0, 0);
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * 30, 0, 0);
 	}
 }
